fb29c45fa2319d812250d5c14161ec850ab52aa8
Reinhardt Botha <botha.wr@gmail.com>
2024-02-07 23:19:00 -0800

    edited index.js in routes

---
+++ routes/index.ts
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+// Example:
+// const nameRouter = require("./name.routes")
+// router.use(nameRouter)
+
+module.exports = router;
