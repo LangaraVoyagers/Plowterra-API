d6d0e48b65accca015609288e61e77bf6d764bce
BryanCaldeira <bryancaldeira22@gmail.com>
2024-02-16 21:45:33 -0800

    feat: setup backend config for CI/CD and SAM CLI deployments

--- app.ts
+++ app.ts
@@ -7,18 +7,23 @@ import {
   AddressInfo 
 } from 'net';
 import dotenv from 'dotenv'
+import serverless from 'serverless-http';
+import cors from 'cors';
 
 // load environment variables
 dotenv.config();
 
 const app: Application = express();
+app.use(cors());
 const port = Number(process.env.PORT) || 8000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hi we are team Voyagers');
+app.get('/hello', (req: Request, res: Response) => {
+  res.json({"message": "Hi from team Voyagers"});
 });
 
 const listener = app.listen(port, () => {
   const { port } = listener.address() as AddressInfo
   console.log(`Server running on port ${port}`);
 });
+
+export const handler = serverless(app);
