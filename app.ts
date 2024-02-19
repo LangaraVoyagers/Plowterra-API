2bad41f79d600f68d6799afcadfdabc270c4ddfb
ALFAROSO <alberth.sotelo@gmail.com>
2024-02-18 22:23:05 -0800

    update app file

--- app.ts
+++ app.ts
@@ -1,26 +1,23 @@
-import express, { 
-  Request, 
-  Response , 
-  Application 
-} from 'express';
-import { 
-  AddressInfo 
-} from 'net';
-import dotenv from 'dotenv'
+import express, { Request, Response, Application } from 'express';
+import { AddressInfo } from 'net';
+import dotenv from 'dotenv';
+import productRouter from './routes/products.routes'; 
 
-// load environment variables
 dotenv.config();
 
 require('./models/db');
 
 const app: Application = express();
+app.use(express.json());
 const port = Number(process.env.PORT) || 8000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hi we are team Voyagers');
 });
 
+app.use('/api/v1', productRouter);
+
 const listener = app.listen(port, () => {
-  const { port } = listener.address() as AddressInfo
+  const { port } = listener.address() as AddressInfo;
   console.log(`Server running on port ${port}`);
 });
--- controllers/product.controllers.ts
+++ controllers/product.controllers.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import Product, { IProduct } from '../models/Product';
+import Product, { IProduct } from '../models/Products';
 
 export const createProduct = async (req: Request, res: Response) => {
     try {
