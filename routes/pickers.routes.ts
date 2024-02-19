a1e7ed941e6dc1d2de3b569d20d9769022fc71ff
ALFAROSO <alberth.sotelo@gmail.com>
2024-02-18 21:17:40 -0800

    endpoints on pickers.routes file

---
+++ routes/pickers.routes.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import { createPicker, getPicker, getAllPickers, updatePicker, softDeletePicker } from '../controllers/pickerController';
+
+const router = express.Router();
+
+// POST: /api/v1/pickers
+router.post('/pickers', createPicker);
+
+// GET: /api/v1/pickers
+router.get('/pickers', getAllPickers);
+
+// GET: /api/v1/pickers/:id
+router.get('/pickers/:id', getPicker);
+
+// PUT: /api/v1/pickers/:id
+router.put('/pickers/:id', updatePicker);
+
+//SOFT DELETE: /api/v1/pickers/:id
+router.patch('/pickers/:id', softDeletePicker);
+
+export default router;
