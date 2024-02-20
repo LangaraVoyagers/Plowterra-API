9a86bb3ddd9de743e4be87247aeccbcfc64a75b7
ALFAROSO <alberth.sotelo@gmail.com>
2024-02-20 15:49:27 -0800

    feat: add functions on pickers.controllers

--- controllers/picker.controllers.ts
+++ controllers/picker.controllers.ts
@@ -1,7 +1,58 @@
-// TODO: require the model
-// Picker = require("../models/Picker")
+import { Request, Response } from 'express';
+import Picker, { IPicker } from '../models/Picker';
 
-// TODO: add controller logic 
+export const createPicker = async (req: Request, res: Response) => {
+    try {
+        const pickerData: IPicker = req.body;
+        const picker = new Picker(pickerData);
+        await picker.save();
+        res.status(201).json(picker);
+    } catch (error: any) {
+        res.status(500).json({ error: error.message });
+    }
+};
 
-// TODO: export module
-// module.exports = { controllerName1, controllerName2, etc};
+export const getPicker = async (req: Request, res: Response) => {
+    try {
+        const picker = await Picker.findById(req.params.id);
+        if (!picker) {
+            return res.status(404).json({ error: 'Picker not found' });
+        }
+        res.json(picker);
+    } catch (error: any) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+export const getAllPickers = async (req: Request, res: Response) => {
+    try {
+        const activePickers = await Picker.find({ deletedAt: null });
+        res.json(activePickers);
+    } catch (error: any) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+export const updatePicker = async (req: Request, res: Response) => {
+    try {
+        const updatedPicker = await Picker.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updatedPicker) {
+            return res.status(404).json({ error: 'Picker not found' });
+        }
+        res.json(updatedPicker);
+    } catch (error: any) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+export const softDeletePicker = async (req: Request, res: Response) => {
+    try {
+        const updatedPicker = await Picker.findByIdAndUpdate(req.params.id, { deletedAt: Date.now() });
+        if (!updatedPicker) {
+            return res.status(404).json({ error: 'Picker not found' });
+        }
+        res.json({ message: 'Picker deleted successfully' });
+    } catch (error: any) {
+        res.status(500).json({ error: error.message });
+    }
+}
