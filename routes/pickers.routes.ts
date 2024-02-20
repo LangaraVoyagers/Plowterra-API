9801daeaec603e649e1f6df2c1f7f7b9664543d6
ALFAROSO <alberth.sotelo@gmail.com>
2024-02-20 15:42:20 -0800

    feat: add endpoints to the pickers.routers file

--- routes/pickers.routes.ts
+++ routes/pickers.routes.ts
@@ -1,8 +1,23 @@
 import express from "express";
+import { createPicker, getPicker, getAllPickers, updatePicker, softDeletePicker } from '../controllers/picker.controllers';
+
 const router = express.Router();
 
 const pickerCtrl = require("../controllers/picker.controllers");
 
-// TODO: Picker endpoints
+// POST: /api/v1/pickers
+router.post('/pickers', createPicker);
+
+// GET: /api/v1/pickers
+router.get('/pickers', getAllPickers);
+
+// GET: /api/v1/pickers/:id
+router.get('/pickers/:id', getPicker);
+
+// PUT: /api/v1/pickers/:id
+router.put('/pickers/:id', updatePicker);
+
+//SOFT DELETE: /api/v1/pickers/:id
+router.patch('/pickers/:id', softDeletePicker);
 
 module.exports = router;
