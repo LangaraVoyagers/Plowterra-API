f8b790f97e776948a5be0d2befd7847d30dda680
Reinhardt Botha <botha.wr@gmail.com>
2024-02-20 20:27:51 -0800

    chore: added comments in routes

--- routes/seasons.routes.ts
+++ routes/seasons.routes.ts
@@ -4,11 +4,22 @@ import paths from "../shared/paths";
 
 const seasonRouter = express.Router();
 
+// GET: /api/v1/seasons
 seasonRouter.get(paths.season, seasonController.getAll);
+
+// GET: /api/v1/seasons/:id
 seasonRouter.get(`${paths.season}/:id`, seasonController.getById);
+
+// POST: /api/v1/seasons
 seasonRouter.post(paths.season, seasonController.create);
+
+// PUT: /api/v1/seasons/:id
 seasonRouter.put(`${paths.season}/:id`, seasonController.update);
+
+// SOFT DELETE: /api/v1/seasons/:id
 seasonRouter.delete(`${paths.season}/:id`, seasonController.remove);
+
+// PUT: /api/v1/seasons/:id/close
 seasonRouter.put(`${paths.season}/:id/close`, seasonController.close);
 
 export default seasonRouter;
