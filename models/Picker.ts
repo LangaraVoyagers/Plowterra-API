b7290d6149df7cf4abf0fef394cd7281f48c5d51
ALFAROSO <alberth.sotelo@gmail.com>
2024-02-22 10:07:52 -0800

    fix: Group properties in a different way in file Pickers.ts

--- models/Picker.ts
+++ models/Picker.ts
@@ -3,35 +3,39 @@ import mongoose, { Schema } from 'mongoose';
 export interface IPicker {
     name: string;
     phoneNumber: string;
-    emergencyPhoneNumber: string;
-    relationToPicker: string;
+    emergencyContact: {
+        name: string;
+        phoneNumber: string;
+        relationToPicker: string;
+    };
     govId: string;
     address: string;
     bloodType: string;
     status: boolean;
-    warningsCount: number;
     score: number;
-    createdBy: string;
-    createdAt: Date;
-    employmentStartDate: Date;
-    employmentEndDate: Date;
+    employment: {
+        startDate: Date;
+        endDate: Date;
+    };
 }
 
 const PickerSchema: Schema = new Schema({
     name: { type: String, required: true, maxlength: 40 },
     phoneNumber: { type: String, required: true, maxlength: 15 },
-    emergencyPhoneNumber: { type: String, required: true, maxlength: 15 },
-    relationToPicker: { type: String, required: true, maxlength: 10 },
+    emergencyContact: {
+        name: { type: String, required: true, maxlength: 40 },
+        phoneNumber: { type: String, required: true, maxlength: 15 },
+        relationToPicker: { type: String, required: true, maxlength: 10 }
+    },
     govId: { type: String, required: true, maxlength: 20 },
     address: { type: String, required: true, maxlength: 50 },
     bloodType: { type: String, required: true, maxlength: 5 },
     status: { type: Boolean, required: true, default: true },
-    warningCount: { type: Number, required: true, default: 0 },
     score: { type: Number, required: true, default: 0 },
-    createdBy: { type: String, required: true, maxlength: 40 },
-    createdAt: { type: Date, required: true, default: Date.now },
-    employmentStartDate: { type: Date, required: true, default: Date.now },
-    employmentEndDate: { type: Date, required: true, default: Date.now },
+    employment: {
+        startDate: { type: Date, required: true, default: Date.now },
+        endDate: { type: Date, required: true, default: Date.now }
+    }
 });
 
 const Picker = mongoose.model<IPicker>('Picker', PickerSchema);
