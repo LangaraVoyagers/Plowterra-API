096e7db2ecef1628098a352e177cead41d378cab
Wonnnyo Hamester <wonnyohamester@outlook.com>
2024-02-23 20:53:42 -0800

    refactor(pickers): change controller responses

--- controllers/picker.controllers.ts
+++ controllers/picker.controllers.ts
@@ -1,87 +1,138 @@
-import { Request, Response } from 'express';
-import Picker, { IPicker } from '../models/Picker';
+import { Request, Response } from "express";
+import Picker, { IPicker } from "../models/Picker";
+import Message from "../shared/Message";
+import getContentLocation from "../shared/get-content-location";
+
+const message = new Message("picker");
 
 export function createPicker(req: Request, res: Response) {
-    const pickerData: IPicker = req.body;
-    const picker = new Picker(pickerData);
-  
-    picker.save()
-      .then((createdPicker) => {
-        res.status(201).json({
-          message: 'Picker created successfully',
-          data: createdPicker,
-          error: null,
+  const pickerData: IPicker = req.body;
+  const picker = new Picker(pickerData);
+
+  picker
+    .save()
+    .then((data) => {
+      const url = getContentLocation(req, data._id);
+
+      res
+        .set("content-location", url)
+        .status(201)
+        .json({
+          data,
+          error: false,
+          message: message.create("success"),
         });
-      })
-      .catch((error) => {
-              res.status(500).json({
-          message: 'Error creating picker',
-          data: null,
-          error,
-        });
+    })
+    .catch(() => {
+      res.status(500).json({
+        data: null,
+        error: true,
+        message: message.create("error"),
       });
-  }
+    });
+}
 
 export function getPicker(req: Request, res: Response) {
   Picker.findById(req.params.id)
-    .then((picker) => {
-      if (!picker) {
-        return res.status(404).json({ error: 'Picker not found' });
+    .then((data) => {
+      if (!data) {
+        return res
+          .status(404)
+          .json({ data, error: true, message: message.get("not_found") });
       }
-      res.json(picker);
+
+      res.status(200).json({
+        data,
+        error: false,
+        message: message.delete("success"),
+      });
     })
-    .catch((error) => {
-      res.status(500).json({ error });
+    .catch(() => {
+      res.status(500).json({
+        data: null,
+        error: true,
+        message: message.delete("error"),
+      });
     });
-};
+}
 
 export function getAllPickers(req: Request, res: Response) {
   Picker.find({ deletedAt: null })
-    .then((activePickers) => {
-      res.json(activePickers);
+    .then((data) => {
+      res.status(200).json({
+        data,
+        error: false,
+        message: message.get("success"),
+      });
     })
-    .catch((error) => {
-      res.status(500).json({ error });
+    .catch(() => {
+      res.status(500).json({
+        data: null,
+        error: true,
+        message: message.get("error"),
+      });
     });
 }
 
 export function updatePicker(req: Request, res: Response) {
-    Picker.findByIdAndUpdate(req.params.id, req.body, { new: true })
-      .then((updatedPicker) => {
-        if (!updatedPicker) {
-          return res.status(404).json({ error: 'Picker not found' });
-        }
-        res.status(200).json({
-          message: "Picker updated",
-          data: updatedPicker,
-          error: null,
-        });
-      })
-      .catch((error) => {
-        res.status(500).json({
-          message: "Error updating picker",
-          data: null,
-          error,
-        });
-      });
-  };
+  const id = req?.params?.id;
 
-export function softDeletePicker(req: Request, res: Response) {
-  Picker.findByIdAndUpdate(req.params.id, { deletedAt: Date.now() })
-    .then((updatedPicker) => {
-      if (!updatedPicker) {
-        return res.status(404).json({ error: 'Picker not found' });
+  Picker.findOneAndUpdate({ _id: id, deletedAt: null }, req.body, {
+    returnDocument: "after",
+  })
+    .then((data) => {
+      if (!data) {
+        return res
+          .status(404)
+          .json({ data, error: true, message: message.get("not_found") });
       }
+
       res.status(200).json({
-        message: 'Picker deleted successfully',
-        data: updatedPicker ,
-        error: null,
+        data,
+        error: false,
+        message: message.update("success"),
       });
     })
-    .catch((error) => {
+    .catch(() => {
       res.status(500).json({
-        message: 'Error deleting picker' ,
         data: null,
-        error,});
+        error: true,
+        message: message.update("error"),
+      });
     });
-};
+}
+
+export function softDeletePicker(req: Request, res: Response) {
+  const id = req?.params?.id;
+
+  Picker.findOneAndUpdate(
+    { _id: id, deletedAt: null },
+    {
+      deletedAt: new Date().getTime(),
+      deletedBy: "",
+    },
+    {
+      returnDocument: "after",
+    }
+  )
+    .then((data) => {
+      if (!data) {
+        return res
+          .status(404)
+          .json({ data, error: true, message: message.get("not_found") });
+      }
+
+      res.status(200).json({
+        data,
+        error: false,
+        message: message.delete("success"),
+      });
+    })
+    .catch(() => {
+      res.status(500).json({
+        data: null,
+        error: true,
+        message: message.delete("error"),
+      });
+    });
+}
