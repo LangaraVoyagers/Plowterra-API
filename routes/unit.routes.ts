32befabe2cf2bc42972e071bbfe147e4ff866578
ALFAROSO <alberth.sotelo@gmail.com>
2024-02-28 15:06:03 -0800

    fIX: routes for unit

--- routes/unit.routes.ts
+++ routes/unit.routes.ts
@@ -1,15 +1,19 @@
 import express from "express";
-import unitController from "../controllers/unit.controllers"; // Importa el controlador de unidades
+import unitController from "../controllers/unit.controllers"; 
 import paths from "../shared/paths";
 
-const router = express.Router();
+const router = express.Router()
 
-router.get(paths.unit, unitController.getAll); // Utiliza el controlador de unidades para las rutas relacionadas con unidades
-
-router.get(`${paths.unit}/:id`, unitController.getById);
-
+// POST: /api/v1/units
 router.post(paths.unit, unitController.create);
 
+// GET: /api/v1/units
+router.get(paths.unit, unitController.getAll);
+
+// GET: /api/v1/units/:id
+router.get(`${paths.unit}/:id`, unitController.getById);
+
+// DELETE: /api/v1/units/:id
 router.delete(`${paths.unit}/:id`, unitController.remove);
 
 export default router;
