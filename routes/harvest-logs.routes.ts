a30936f725e5b060591613a2792e2cc7d694265b
Reinhardt Botha <botha.wr@gmail.com>
2024-03-04 19:59:13 -0800

    chore: update project-2-types version

--- routes/harvest-logs.routes.ts
+++ routes/harvest-logs.routes.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import { harvestLogValidator } from "project-2-types/lib/harvestLog.ajv";
 import harvestController from "../controllers/harvest-log.controllers";
 import paths from "../shared/paths";
-import { harvestLogValidator} from "project-2-types/lib/harvestLog.ajv"
 const harvestLogRouter = express.Router();
 
 // POST: /api/v1/harvest-logs
-harvestLogRouter.post(paths.harvestLog, harvestLogValidator, harvestController.create);
+harvestLogRouter.post(
+  paths.harvestLog,
+  harvestLogValidator,
+  harvestController.create
+);
 
 // GET: /api/v1/harvest-logs
 harvestLogRouter.get(paths.harvestLog, harvestController.getAll);
