d9b8747b4b7272fdf3b388ba55c0ab845a786670
Reinhardt Botha <botha.wr@gmail.com>
2024-03-04 19:40:07 -0800

    feat: add post validator

--- routes/harvest-logs.routes.ts
+++ routes/harvest-logs.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import harvestController from "../controllers/harvest-log.controllers";
 import paths from "../shared/paths";
+import { harvestLogValidator} from "project-2-types/lib/harvestLog.ajv"
 const harvestLogRouter = express.Router();
 
 // POST: /api/v1/harvest-logs
-harvestLogRouter.post(paths.harvestLog, harvestController.create);
+harvestLogRouter.post(paths.harvestLog, harvestLogValidator, harvestController.create);
 
 // GET: /api/v1/harvest-logs
 harvestLogRouter.get(paths.harvestLog, harvestController.getAll);
