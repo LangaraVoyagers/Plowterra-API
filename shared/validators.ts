5034a01e9dca746f65ae670e1a598955a0045957
Reinhardt Botha <botha.wr@gmail.com>
2024-03-05 10:20:26 -0800

    chore: move Express validator from module to repo

--- shared/validators.ts
+++ shared/validators.ts
@@ -1,20 +1,20 @@
 import Ajv from "ajv";
-import { Request, Response, NextFunction } from "express";
+import { NextFunction, Request, Response } from "express";
 import schema from "project-2-types/lib/harvestLog.ajv";
 
 const ajv = new Ajv();
 
 const harvestLogValidator = (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    const validate = ajv.compile(schema);
-    const valid = validate(req.body);
-    if (!valid) {
-      return res.status(400).json(validate.errors);
-    }
-    next();
-  };
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const validate = ajv.compile(schema);
+  const valid = validate(req.body);
+  if (!valid) {
+    return res.status(400).json(validate.errors);
+  }
+  next();
+};
 
 export default harvestLogValidator;
