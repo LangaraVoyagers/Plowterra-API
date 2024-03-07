0b7755092d4b366717cd29c3439aab835f004211
Wonnnyo Hamester <wonnyohamester@outlook.com>
2024-03-06 17:39:29 -0800

    feat: picker validations

--- shared/validators.ts
+++ shared/validators.ts
@@ -1,20 +1,30 @@
 import Ajv from "ajv";
 import { NextFunction, Request, Response } from "express";
-import schema from "project-2-types/lib/harvestLog.ajv";
 
-const ajv = new Ajv();
+import addErrors from "ajv-errors";
+import addFormats from "ajv-formats";
+import addKeywords from "ajv-keywords";
 
-const harvestLogValidator = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const validate = ajv.compile(schema);
-  const valid = validate(req.body);
-  if (!valid) {
-    return res.status(400).json(validate.errors);
-  }
-  next();
+const ajv = new Ajv({
+  useDefaults: "empty",
+  allErrors: true,
+  coerceTypes: true,
+});
+
+addFormats(ajv, ["date", "email"]);
+addErrors(ajv);
+addKeywords(ajv, "transform");
+
+export const validator = (schema: any) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const validate = ajv.compile(schema);
+    const valid = validate(req.body);
+
+    if (!valid) {
+      return res.status(400).json(validate.errors);
+    }
+    next();
+  };
 };
 
-export default harvestLogValidator;
+export default validator;
